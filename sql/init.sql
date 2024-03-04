a1c9af6cd924fc9424700aa9b61bd74db24b54fc
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2024-03-04 12:57:07 +0100

    feat: implement seed checkpoint to improve the indexing time

--- sql/init.sql
+++ sql/init.sql
@@ -8,3 +8,6 @@ CREATE INDEX IF NOT EXISTS yieldDex_tm_transfer_constractaddress ON yielddex_dep
 CREATE INDEX IF NOT EXISTS yieldDex_tm_transfer_from ON yielddex_deposit("from");
 CREATE INDEX IF NOT EXISTS yieldDex_tm_transfer_to ON yielddex_deposit("to");
 CREATE INDEX IF NOT EXISTS yieldDex_tm_transfer_timestamp ON yielddex_deposit("timestamp");
+
+-- Set _checkpoints id not null
+ALTER TABLE "_checkpoints" ADD "id" character varying NOT NULL

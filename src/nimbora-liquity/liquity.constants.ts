abbcc64227ba5789bc89bf91b12a706649d856e9
ioanSL <io@shardlabs.io>
2024-03-05 16:01:07 +0200

    Fix contract addresses in liquity.constants.ts

--- src/nimbora-liquity/liquity.constants.ts
+++ src/nimbora-liquity/liquity.constants.ts
@@ -15,13 +15,13 @@ export const lusdEvents = [
 const liquityAddressesMainnet = [
   {
     name: 'trove_275',
-    contract: '0x03580a65260563b5511ddf2eafb83d6b309dce7fc25271df8c040a437f09a399',
+    contract: '0x3580a65260563b5511ddf2eafb83d6b309dce7fc25271df8c040a437f09a399',
     start: 553937,
     events: LiquityEvents,
   },
   {
     name: 'trove_400',
-    contract: '0x02a67288e48a8c4e2881aee422da7841fc11fef195e0a81f929871c77f07509d',
+    contract: '0x2a67288e48a8c4e2881aee422da7841fc11fef195e0a81f929871c77f07509d',
     start: 553937,
     events: LiquityEvents,
   },
